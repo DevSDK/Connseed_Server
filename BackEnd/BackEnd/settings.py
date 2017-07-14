c5ad33656094bb387c384b71691ef6e6669fd1a1
송석호 <k_sdk@naver.com>
2017-07-14 17:39:20 +0900

    allow host

--- BackEnd/BackEnd/settings.py
+++ BackEnd/BackEnd/settings.py
@@ -29,7 +29,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+'169.56.126.151',
+]
 
 
 # Application definition
